bd03ae6f0e15996010eb138549d4d8a60369e999
song <1334359917@qq.com>
2019-08-15 18:25:15 +0800

    url添加前缀

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from hero.view import heroR
 
 app = Flask(__name__)
 
-app.register_blueprint(bookR)
-app.register_blueprint(heroR)
+app.register_blueprint(blueprint=bookR, url_prefix='/v1')
+app.register_blueprint(blueprint=heroR, url_prefix='/v1')
+
+@app.route('/v1/ping')
+def pong():
+    return 'pong!'
 
 if __name__ == '__main__':
    app.run()
